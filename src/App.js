721e8258886024108ee7f17fcdf63890c879a4d5
Russ yev <ruslanyev@yahoo.com>
2019-07-20 15:33:26 -0700

    created routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { GOOGLE_API_KEY } from "./config";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Router>
+        <div className="App">
+          <Route path="/" component={NavBar} />
+
+          <Switch>
+            <Route exact path="/" component={BusinessListings} />
+            <Route exact path="/:businessName" component={BusinessDetails} />
+          </Switch>
+        </div>
+      </Router>
+    </main>
   );
 }
 
