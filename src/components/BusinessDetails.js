e3bb9fdfba6b36780e26fd269043122abc8fa4d6
Russ yev <ruslanyev@yahoo.com>
2019-07-20 15:40:18 -0700

    initial commit

---
+++ src/components/BusinessDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BusinessDetails.css"
+
+export default function BusinessDetails() {
+  return (
+    <div>
+      <h1>The BusinessDetails</h1>
+    </div>
+  )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NavBar.css"
+
+export default function NavBar() {
+  return (
+    <div>
+      <h1>The NavBar</h1>
+    </div>
+  )
+}
