e547db3e166c1b960e4849ae04a5fa05fd3f9314
Russ yev <ruslanyev@yahoo.com>
2019-07-21 00:44:20 -0700

    fetching all data and displaying it

--- src/components/BusinessListings.js
+++ src/components/BusinessListings.js
@@ -1,10 +1,39 @@
 import React from "react";
-import "./BusinessDetails.css"
+import { connect } from "react-redux";
+import "./BusinessListings.css";
 
-export default function BusinessDetails() {
+const BusinessListings = (props) => {
+  const viewBusiness = e => {
+    e.preventDefault();
+    let business = encodeURIComponent(e.target.id)
+    props.history.push(`/${business}`);
+  };
+
   return (
     <div>
-      <h1>The BusinessDetails</h1>
+      {props.fetchingData ? (
+        <h1>Fetching Data</h1>
+      ) : props.serverMessage ? (
+        <h1>{props.serverMessage}</h1>
+      ) : (
+        props.restaurants.map((restaurant, index) => (
+          <div key={index} id={restaurant.name} onClick={viewBusiness}>
+            <h2>{restaurant.name}</h2>
+            <p>{restaurant.category}</p>
+          </div>
+        ))
+      )}
     </div>
-  )
+  );
 }
+
+const mapStateToProps = state => ({
+  restaurants: state.app.restaurants,
+  fetchingData: state.app.fetchingData,
+  serverMessage: state.app.serverMessage
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(BusinessListings);
