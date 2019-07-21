2a6a1252946ad8bc0cf078585a391aa3f4a45d60
Russ yev <ruslanyev@yahoo.com>
2019-07-21 00:44:54 -0700

    displaying all business details

--- src/components/BusinessDetails.js
+++ src/components/BusinessDetails.js
@@ -1,10 +1,71 @@
-import React from "react";
-import "./BusinessDetails.css"
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import "./BusinessDetails.css";
+// import { GOOGLE_API_KEY } from "./config";
 
-export default function BusinessDetails() {
+const BusinessDetails = props => {
+  const [business, setBusiness] = useState();
+
+  useEffect(() => {
+    const businessName = decodeURIComponent(
+      props.location.pathname.substr(
+        props.location.pathname.lastIndexOf("/") + 1
+      )
+    );
+
+    const findBusiness = business => {
+      return business.name === businessName;
+    };
+
+    const businessDetsails = props.restaurants.find(findBusiness);
+    setBusiness(businessDetsails);
+  }, [props.location.pathname, props.restaurants]);
+
   return (
     <div>
-      <h1>The BusinessDetails</h1>
+      {business === undefined ? (
+        <h1>Fetching Data</h1>
+      ) : props.serverMessage ? (
+        <h1>{props.serverMessage}</h1>
+      ) : (
+        <div>
+          <div className="nameCategory">
+            <h2>{business.name}</h2>
+            <p>{business.category}</p>
+          </div>
+
+          <div className="address">
+            <p>
+              {business.location.address}
+              <br />
+              {business.location.city}, {business.location.state}{" "}
+              {business.location.postalCode}
+            </p>
+          </div>
+
+          {business.contact ? (
+            <div className="contactInfo">
+              {business.contact.formattedPhone ? (
+                <p>{business.contact.formattedPhone}</p>
+              ) : null}
+              {business.contact.twitter ? (
+                <p>@{business.contact.twitter}</p>
+              ) : null}
+            </div>
+          ) : null}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
+
+const mapStateToProps = state => ({
+  restaurants: state.app.restaurants,
+  fetchingData: state.app.fetchingData,
+  serverMessage: state.app.serverMessage
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(BusinessDetails);
