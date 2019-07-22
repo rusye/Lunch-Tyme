b2f0383307185c734d8d23eca7e1774965711f49
Russ yev <ruslanyev@yahoo.com>
2019-07-21 17:26:15 -0700

    finished navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,28 @@
 import React from "react";
-import "./NavBar.css"
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import mapIcon from "../icons/icon_map@2x.png";
+import backIcon from "../icons/ic_webBack@2x.png";
 
-export default function NavBar() {
+export default function NavBar(props) {
+  const home = "/";
+  const location = props.location.pathname;
+
   return (
-    <div>
-      <h1>The NavBar</h1>
-    </div>
-  )
+    <nav>
+      {location !== home ? (
+        <Link tabIndex="-1" to="/">
+          <img className="icon" src={backIcon} alt="back" />
+        </Link>
+      ) : (
+        <img className="icon home" src={backIcon} alt="back" />
+      )}
+
+      <h1 className="name">Lunch Tyme</h1>
+
+      <Link tabIndex="-1" to="/">
+        <img className="icon" src={mapIcon} alt="map" />
+      </Link>
+    </nav>
+  );
 }
