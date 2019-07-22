36e90dd8fb5e497b16127f6ad724f138b27c69da
Russ yev <ruslanyev@yahoo.com>
2019-07-21 23:39:58 -0700

    finished styling for listings

--- src/components/BusinessListings.js
+++ src/components/BusinessListings.js
@@ -2,30 +2,46 @@ import React from "react";
 import { connect } from "react-redux";
 import "./BusinessListings.css";
 
-const BusinessListings = (props) => {
+const BusinessListings = props => {
   const viewBusiness = e => {
     e.preventDefault();
-    let business = encodeURIComponent(e.target.id)
+    let business = encodeURIComponent(e.target.id);
     props.history.push(`/${business}`);
   };
 
   return (
-    <div>
+    <div className="businessListings row">
       {props.fetchingData ? (
         <h1>Fetching Data</h1>
       ) : props.serverMessage ? (
         <h1>{props.serverMessage}</h1>
       ) : (
         props.restaurants.map((restaurant, index) => (
-          <div key={index} id={restaurant.name} onClick={viewBusiness}>
-            <h2>{restaurant.name}</h2>
-            <p>{restaurant.category}</p>
+          <div
+            key={index}
+            id={restaurant.name}
+            onClick={viewBusiness}
+            className="listingBefore col-6"
+          >
+            <div
+              className="listing"
+              style={{
+                backgroundImage: `url(${restaurant.backgroundImageURL})`
+              }}
+            >
+              <div className="text">
+                <h2>{restaurant.name}</h2>
+                <p>{restaurant.category}</p>
+              </div>
+
+              <div className="gradient" />
+            </div>
           </div>
         ))
       )}
     </div>
   );
-}
+};
 
 const mapStateToProps = state => ({
   restaurants: state.app.restaurants,
