5a3163abb0cf134dcfc8e4dfc4e8c075d9d9a940
Russ yev <ruslanyev@yahoo.com>
2019-07-22 00:26:29 -0700

    re-read the instructions regarding footer and detail view

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 import mapIcon from "../icons/icon_map@2x.png";
-import backIcon from "../icons/ic_webBack@2x.png";
 
-export default function NavBar(props) {
-  const home = "/";
-  const location = props.location.pathname;
-
+export default function NavBar() {
   return (
     <header>
       <nav>
-        {location !== home ? (
-          <Link tabIndex="-1" to="/">
-            <img className="icon" src={backIcon} alt="back" />
-          </Link>
-        ) : (
-          <img className="icon home" src={backIcon} alt="back" />
-        )}
-
         <h1 className="name">Lunch Tyme</h1>
 
         <Link tabIndex="-1" to="/">
